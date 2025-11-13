036b34af6a04349ad5ac2de937666feebe4dcc57
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-13 11:21:28 +0300

    Create part-w-0001-why.vue

---
+++ part-w-0001-why.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+  let whyVue = "Vue for web3 apps!.."
+
+  let years = "2026 & 2027"
+
+
+</script>
+
+<template>
+
+  <h2>{{ whyVue }}</h2>
+
+  <h3>Years: {{ years}}</h3>
+
+</template>
+
