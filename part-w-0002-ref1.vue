02c388e344aedcc61611e7027a28967c8982268f
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-13 11:30:01 +0300

    Create part-w-0002-ref1.vue

---
+++ part-w-0002-ref1.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+  import { ref } from 'vue';
+
+  let counter = ref(0)
+
+  function counterPlus() {
+
+    counter.value++
+  }
+
+
+</script>
+
+<template>
+
+  <h2>Counter: {{ counter }}</h2>
+
+  <button @click="counterPlus">Counter ++</button>
+
+</template>
+
