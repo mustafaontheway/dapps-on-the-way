03fd2a94b66f331fa6a47bb8467b3193ba7ceb03
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-17 21:02:31 +0300

    Create part-x-0005-nested-mapping1.sol

---
+++ part-x-0005-nested-mapping1.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    // department id, employee id, employee name
+
+    mapping(uint8 => mapping(uint16 => string)) public employees;
+
+    function setEmployee(uint8 _depID, uint16 _empID, string memory _name) public {
+
+        employees[_depID][_empID] = _name;
+    }
+
+
+    function getEmployeeName(uint8 _depID, uint16 _empID) public view returns (string memory) {
+
+        return employees[_depID][_empID];
+    }
+
+}
+
