3bfb0b291d12453a47ba23803a14a33809a64b41
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-17 21:12:28 +0300

    Create part-x-0006-bool-logic1.sol

---
+++ part-x-0006-bool-logic1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    bool public ready = true;
+
+    bool public completed = false;
+
+    bool public comparision1 = 3 != 4;
+
+    bool public comparision2 = 2 == 4;
+
+    bool public comparision3 = (5 > 4) && (11 < 99);
+
+    bool public comparision4 = (-4 <= -13) || (12 >= 99);
+
+}
+
