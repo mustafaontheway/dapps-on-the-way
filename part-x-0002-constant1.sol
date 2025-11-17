0a334f5f10a314c90a8a5b33a0e24f2912d4ad0c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-17 19:35:22 +0300

    Create part-x-0002-constant1.sol

---
+++ part-x-0002-constant1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    uint16 public constant initialFixedCostUSD = 45_000;
+
+    string public constant version = "DeFi.2025.12.01";
+
+}
