344af35fe219be6497141f32cfeb433c8edbbf3c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-17 19:39:35 +0300

    Update part-x-0003-immutable1.sol

--- part-x-0003-immutable1.sol
+++ part-x-0003-immutable1.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    uint16 public immutable initialFixedCostUSD;
+
+    uint32 public immutable expectedSalesTarget;
+
+    constructor(uint16 _fc, uint32 _est) {
+
+        initialFixedCostUSD = _fc;
+
+        expectedSalesTarget = _est;
+    }
+
+}
+
 
