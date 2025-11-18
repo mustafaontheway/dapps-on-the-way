f324ccaa2a69ca46a7fa9946f4c00c03faff79a1
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-18 18:36:24 +0300

    Create part-x-0013-concat1.sol

---
+++ part-x-0013-concat1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    string public company = "Akis Company";
+
+    string public capitalUSD = "350_000 USD";
+
+    function returnCompanyInfo() public view returns (string memory) {
+
+        return string.concat("Name: ",company, " and capital: " ,capitalUSD); // Name: Akis Company and capital: 350_000 USD
+    }
+
+}
+
