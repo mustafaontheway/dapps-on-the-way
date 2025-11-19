df72e2bdacafa5212acb0722727d8d744aeddbcf
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-11-19 18:37:54 +0300

    Create part-x-0018-wei-gwei1.sol

---
+++ part-x-0018-wei-gwei1.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Mustafa {
+
+    bool public etherToGwei = 1 ether == 1e9 gwei; // true, 10 ^ 9
+
+    bool public etherToWei = 1 ether == 1e18 wei; // true, 10 ^ 18
+
+}
+
